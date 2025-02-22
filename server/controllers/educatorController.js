21a1983898bfbe4387b46791e9b85525a922dee9
Anurag25021 <anuragsharma251001@gmail.com>
2025-02-22 19:07:32 +0530

    ew

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,6 +1,7 @@
 import {clerkClient} from '@clerk/express'
 import Course from '../models/course.js'
 import {v2 as cloudinary} from'cloudinary'
+
 export const updateRoleToEducator = async (req,res)=>{
     try{
         const userId=req.auth.userId
@@ -29,12 +30,13 @@ export const addCourse =async(req,res)=>{
 
         const parsedCourseData=await JSON.parse(courseData)
         parsedCourseData.educator=educatorId
-        await Course.create(parsedCourseData)
+        const newCourse = await Course.create(parsedCourseData);
         const imageUpload= await cloudinary.uploader.upload(imageFile.path)
         newCourse.courseThumbnail= imageUpload.secure_url
         await newCourse.save()
         res.json({success:true,message:'Course Added'})
     }catch(error){
-        res.json({success:false,message:"Hello not ower"})
+        res.json({success:false, message: error.message});
     }
+    
 }
