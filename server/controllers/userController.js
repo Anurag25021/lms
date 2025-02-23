44b21d9638e43d87945a04c90ec582b68878e63a
Anurag25021 <anuragsharma251001@gmail.com>
2025-02-23 13:05:52 +0530

    dfd

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,8 @@
+import mongoose from "mongoose"
+import Course from "../models/course.js"
 import User from "../models/User.js"
+import { Purchase } from "../models/purchase.js"
+import Stripe from "stripe"
 export const getUserData = async(req,res)=>{
     try{
         const userId=req.auth.userId
@@ -22,5 +26,53 @@ export const userEnrolledCourses=async(req,res)=>{
     }catch(error){
         res.json({success:false,message:error.message})
 
+    }
+}
+
+
+export const purchaseCourse =async(req,res)=>{
+    try{
+        const {courseId}=req.body
+        const {origin}=req.headers
+        const userId=req.auth.userId
+        const userData=await User.findById(userId)
+        const courseData=await Course.findById(courseId)
+        if(!userData || !courseData){
+            return res.json({success:false,message:'Data Not Found'})
+        }
+        const purchaseData={
+            courseId:courseData._id,
+            userId,
+            amount:(courseData.coursePrice-courseData.discount*courseData.coursePrice/100).toFixed(2),
+
+        }
+        const newPurchase =await Purchase.create(purchaseData)
+
+        //stripe
+        const stripeInstance =new Stripe(process.env.STRIPE_SECRET_KEY)
+        const currency=process.env.CURRENCY.toLowerCase()
+        const line_items=[{
+            price_data:{
+                currency,
+                product_data:{
+                    name:courseData.courseTitle
+
+                },
+                unit_amount:Math.floor(newPurchase.amount)*100
+            },
+            quantity:1
+        }]
+        const session =await stripeInstance.checkout.sessions.create({
+            success_url:`${origin}/loading/my-enrollements`,
+            cancel_url:`${origin}/`,
+            line_items:line_items,
+            mode:'payment',
+            metadata:{
+                purchaseId:newPurchase._id.toString()
+            }
+        })
+        res.json({success:true,success_url:session.url})
+    }catch(error){
+        res.json({success:false,message:error.message})
     }
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,9 @@
-import { getUserData, userEnrolledCourses } from "../controllers/userController.js"
+import { getUserData, purchaseCourse, userEnrolledCourses } from "../controllers/userController.js"
 import express from 'express'
 const userRouter=express.Router()
 
 userRouter.get('/data',getUserData)
 userRouter.get('/enrolled-courses',userEnrolledCourses)
+userRouter.post('/purchase',purchaseCourse)
 
 export default userRouter
