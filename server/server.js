2f017008f1d8b08e3f16a977f4f77e0eb6c6b1af
Anurag25021 <anuragsharma251001@gmail.com>
2025-03-29 15:48:59 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -2,21 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js'
-const app = express();
+import { clerkWebhooks } from './controllers/webhooks.js';
 
+const app = express();
 app.use(cors());
+app.use(express.json()); // Middleware for JSON parsing
 
 // Connect to database
-connectDB().then(() => {
-    console.log("MongoDB Connected");
-}).catch(err => {
-    console.error("Database connection error:", err);
-});
+connectDB();
 
 // Define API route
 app.get('/', (req, res) => res.send('API working'));
-app.post('/clerk',express.json(),clerkWebhooks)
+app.post('/clerk', clerkWebhooks);
 
 // Start server
 const PORT = process.env.PORT || 5000;
